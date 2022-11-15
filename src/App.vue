b39a1cce8182f1024d476d363c78e26eb7f3959d
17axah <17axah@mail.ru>
2022-11-15 18:22:33 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import AppLayout from '@/components/app/Layout.vue'
+</script>
+
+<template>
+  <app-layout>
+    <template #header>
+      header
+    </template>
+    <template #main>
+      main
+    </template>
+    <template #footer>
+      footer
+    </template>
+  </app-layout>
+</template>
